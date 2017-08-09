8e3c98a7e963fc7239fb3d9e1367dd19adabd14f
danremel <remel.dan@gmail.com>
2017-08-09 12:27:32 -0400

    Created routing in React

---
+++ client/src/components/Game.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Game extends Component {
+    render(){
+        return(
+            <div>
+                Hello from Game
+            </div>
+        );
+    }
+}
+
+export default Game;
